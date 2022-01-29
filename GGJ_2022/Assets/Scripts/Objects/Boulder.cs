7e477b701a842660c3631de4924b64a71c7bde62
Jonatan Johansson <smeas@users.noreply.github.com>
2022-01-29 22:28:17 +0100

    Integrate boulder model and make it rotate

--- GGJ_2022/Assets/Scripts/Objects/Boulder.cs
+++ GGJ_2022/Assets/Scripts/Objects/Boulder.cs
@@ -1,9 +1,26 @@
+using System;
 using UnityEngine;
 
 public class Boulder : ShiftBehaviour
 {
 	[SerializeField] private float normalMass = 10;
 	[SerializeField] private float shadowWorldMass = 1;
+	[SerializeField] private Transform modelToRotate;
+
+	private new CircleCollider2D collider;
+
+	protected override void Start()
+	{
+		collider = GetComponent<CircleCollider2D>();
+
+		base.Start();
+	}
+
+	private void Update()
+	{
+		float angle = (transform.position.x / collider.radius) * Mathf.Rad2Deg;
+		modelToRotate.eulerAngles = new Vector3(0f, 0f, -angle);
+	}
 
 	protected override void OnWorldStateChanged(WorldState state)
 	{
