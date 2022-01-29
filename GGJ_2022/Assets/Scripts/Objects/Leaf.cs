c62510f5ba8a2892d30dc217554ad4395660b8af
DreamingJohnny <johnnydreaming@gmail.com>
2022-01-29 12:10:38 +0100

    Synked changes

--- GGJ_2022/Assets/Scripts/Objects/Leaf.cs
+++ GGJ_2022/Assets/Scripts/Objects/Leaf.cs
@@ -3,10 +3,23 @@ using UnityEngine;
 
 public class Leaf : MonoBehaviour
 {
+
+	//So, need way to move... drifting so left to right, but also always downwards.
+	//So, is lerping the way to go? Learp times speed time delta time?
+	//But also, we want to be able to set the edges or maximum values hm... Should that then be a diff from startpoint?
+
+	[SerializeField] private float maxXDiff;
+	[SerializeField] private float fallSpeed;
+
 	private void Start()
 	{
 		ShiftManager.Instance.WorldStateChanged += OnWorldStateChanged;
 		OnWorldStateChanged(ShiftManager.Instance.CurrentWorldState);
+	}
+
+	private void FixedUpdate() {
+		transform.position += new Vector3(0, (fallSpeed * Time.deltaTime));
+		//new Vector3(0, fallSpeed * Time.deltaTime);
 	}
 
 	private void OnWorldStateChanged(WorldState state)
