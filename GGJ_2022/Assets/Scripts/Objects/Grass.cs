2fcadaf67a5adef0242c6ba9ae0f1c35bbf8f856
Jonatan Johansson <smeas@users.noreply.github.com>
2022-01-29 15:27:45 +0100

    Implement grass

--- GGJ_2022/Assets/Scripts/Objects/Grass.cs
+++ GGJ_2022/Assets/Scripts/Objects/Grass.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 public class Grass : ShiftBehaviour
 {
 	[SerializeField] private int damage = 1;
+	[SerializeField] private float knockbackForce = 10f;
 
-	private void OnCollisionEnter2D(Collision2D col)
+	private PlayerHealth playerInTrigger;
+
+	protected override void OnWorldStateChanged(WorldState state)
 	{
-		if (CurrentWorldState == WorldState.ShadowLand &&
-		    col.gameObject.TryGetComponent(out PlayerHealth playerHealth))
+		if (state == WorldState.ShadowLand && playerInTrigger)
 		{
-			playerHealth.TakeDamage(damage);
+			HurtPlayer();
 		}
+	}
+
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.TryGetComponent(out PlayerHealth playerHealth))
+		{
+			playerInTrigger = playerHealth;
+
+			if (CurrentWorldState == WorldState.ShadowLand)
+			{
+				HurtPlayer();
+			}
+		}
+	}
+
+	private void OnTriggerExit2D(Collider2D other)
+	{
+		if (other.gameObject.TryGetComponent(out PlayerHealth _))
+		{
+			playerInTrigger = null;
+		}
+	}
+
+	private void HurtPlayer()
+	{
+		playerInTrigger.TakeDamage(damage);
+
+		Rigidbody2D body = playerInTrigger.GetComponent<Rigidbody2D>();
+		Vector2 knockbackDirection = ((Vector2)playerInTrigger.transform.position - (Vector2)transform.position).normalized;
+		body.AddForce(knockbackDirection * knockbackForce, ForceMode2D.Impulse);
 	}
 }
